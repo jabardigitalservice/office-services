4b668db0d89395ee3112d8fde02a2d289e6faee1
Indra Prasetya <indraprasetya154@gmail.com>
2021-09-20 14:40:58 +0700

    Fix if avatar is null

--- src/app/Models/People.php
+++ src/app/Models/People.php
@@ -29,6 +29,6 @@ class People extends Model
 
     public function getAvatarAttribute()
     {
-        return $this->siapPeople->peg_foto_url;
+        return optional($this->siapPeople)->peg_foto_url;
     }
 }
