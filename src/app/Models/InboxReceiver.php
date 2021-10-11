1fa78bf74285be2e5ea6dd25f930021f4b0a5bd3
samudra-ajri <samudra_ajri@live.com>
2021-10-11 14:54:15 +0700

    Add inbox list by receiver types filtering. The receiver types should be to, to_forward, or cc1.

--- src/app/Models/InboxReceiver.php
+++ src/app/Models/InboxReceiver.php
@@ -77,6 +77,7 @@ class InboxReceiver extends Model
         $urgencies = $filter["urgencies"] ?? null;
         $forwarded = $filter["forwarded"] ?? null;
         $folder = $filter["folder"] ?? null;
+        $receiverTypes = $filter["receiverTypes"] ?? null;
 
         if ($statuses) {
             $arrayStatuses = explode(", ", $statuses);
@@ -131,6 +132,11 @@ class InboxReceiver extends Model
         if ($forwarded || $forwarded == '0') {
             $arrayForwarded = explode(", ", $forwarded);
             $query->whereIn('Status', $arrayForwarded);
+        }
+
+        if ($receiverTypes) {
+            $arrayReceiverTypes = explode(", ", $receiverTypes);
+            $query->whereIn('ReceiverAs', $arrayReceiverTypes);
         }
 
         return $query;
