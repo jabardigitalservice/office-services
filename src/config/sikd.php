b0a728351ff1f41db0173d755c685ff905af63c9
samudra-ajri <samudra_ajri@live.com>
2021-10-15 08:36:43 +0700

    Add signature validation

--- src/config/sikd.php
+++ src/config/sikd.php
@@ -2,7 +2,12 @@
 
 return [
 
+    'url' => env('SIKD_BASE_URL'),
     'base_path_file' => env('SIKD_BASE_PATH_FILE'),
     'base_path_file_signature' => env('SIKD_BASE_PATH_FILE_SIGNATURE'),
+    'base_path_file_letter' => env('SIKD_BASE_PATH_FILE_LETTER'),
+    'signature_url' => env('SIKD_SIGNATURE_URL'),
+    'signature_verify_url' => env('SIKD_SIGNATURE_VERIFY_URL'),
+    'signature_auth' => env('SIKD_SIGNATURE_AUTH'),
 
 ];
