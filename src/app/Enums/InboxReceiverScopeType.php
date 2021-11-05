4c21cf6b56fa7167cffb0d09debe87e147cefd3c
samudra-ajri <samudra_ajri@live.com>
2021-11-05 21:49:56 +0700

    Add inbox receiver scope. This scope will filter the connection between the sender and receiver. REGIONAL = sender-receiver in different department. INTERNAL  = sender-receiver in the same department.

---
+++ src/app/Enums/InboxReceiverScopeType.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Enums;
+
+use Spatie\Enum\Enum;
+
+/**
+ * @method static self REGIONAL()
+ * @method static self INTERNAL()
+ */
+
+final class InboxReceiverScopeType extends Enum
+{
+
+}
