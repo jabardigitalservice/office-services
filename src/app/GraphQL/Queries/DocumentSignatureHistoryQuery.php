9bf7251c7ac127a7e3f12f004b9aa8f37a8a0855
Indra Prasetya <indraprasetya154@gmail.com>
2021-11-15 13:49:51 +0700

    Switch position collection forward and send

--- src/app/GraphQL/Queries/DocumentSignatureHistoryQuery.php
+++ src/app/GraphQL/Queries/DocumentSignatureHistoryQuery.php
@@ -40,8 +40,8 @@ class DocumentSignatureHistoryQuery
                                     ->get();
 
         $data = collect([
-            'documentSignatureSent' => $documentSignatureSent,
             'documentSignatureForward' => $documentSignatureForward,
+            'documentSignatureSent' => $documentSignatureSent,
         ]);
 
         return $data;
