0b1a497c26906087824116e711921038bce66a53
samudra-ajri <samudra_ajri@live.com>
2021-12-13 14:07:30 +0700

    Modify unread counter based on peoplePosition

--- src/config/constants.php
+++ src/config/constants.php
@@ -3,26 +3,34 @@
 return [
 
     'peoplePositionGroups' => [
+        '1' => array(
+            'GUBERNUR'
+        ),
         '2' => array(
             'WAKIL GUBERNUR',
             'SEKRETARIS DAERAH',
-            'KEPALA BIRO',
             'ASISTEN',
-            'KEPALA SEKRETARIAT KOMISI PEMILU',
-            'SEKRETARIS DPRD',
-            'KEPALA INSPEKTORAT',
-            'INSPEKTUR DAERAH',
-            'SEKRETARIS INSPEKTORAT',
-            'KEPALA DINAS',
-            'SEKRETARIS DINAS',
-            'KEPALA BADAN',
-            'KEPALA PELAKSANA HARIAN BADAN',
-            'KEPALA SATUAN POLISI PAMONG PRAJA',
-        ),
-        '9' => array(
+            'KEPALA SEKRETARIAT KOMISI PEMILIHAN UMUM',
+            'KEPALA BIRO',
             'KEPALA BALAI',
             'KEPALA CABANG',
             'KEPALA UPTD',
+            'INSPEKTUR DAERAH',
+            'KEPALA PELAKSANA HARIAN BADAN'
+        ),
+        '3' => array(
+            'KEPALA BADAN',
+            'SEKRETARIS DEWAN PERWAKILAN RAKYAT DAERAH',
+            'KEPALA INSPEKTORAT',
+            'KEPALA DINAS',
+            'KEPALA SATUAN POLISI PAMONG PRAJA'
+        ),
+        '4' => array(
+            'SEKRETARIS DINAS',
+            'SEKRETARIS INSPEKTORAT',
+            'SEKRETARIS BADAN'
+        ),
+        '9' => array(
             'KEPALA UNIT PELAKSANA TEKNIS DAERAH',
             'DIREKTUR'
         )
