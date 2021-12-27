4e475d137e47d64a1628c954a330273f48872845
samudra-ajri <samudra_ajri@live.com>
2021-12-27 13:37:53 +0700

    Clean code

--- src/app/GraphQL/Mutations/InboxReceiverCorrectionMutator.php
+++ src/app/GraphQL/Mutations/InboxReceiverCorrectionMutator.php
@@ -123,7 +123,6 @@ class InboxReceiverCorrectionMutator
         $date = parseDateTimeFormat($dateString, 'dmyhis');
         $title = 'TTE Naskah';
         $body = 'Yah! Ada ' . $draft->type->JenisName . ' terkait dengan ' . $draft->Hal . ' yang harus Anda perbaiki terlebih dahulu. Yuk cek sekarang juga!';
-        // $body = 'Halo terdapat ' . $draft->type->JenisName . ' terkait dengan ' . $draft->Hal . ' yang harus segera Anda Tandatangani. Yuk cek sekarang juga!';
 
         $messageAttribute = [
             'notification' => [
