7840c430c00bcc59799d50710200093e1c47cfef
Indra Prasetya <indraprasetya154@gmail.com>
2022-01-04 14:53:27 +0700

    Create log activity query / mutation

---
+++ src/app/Http/Traits/LogUserActivityTrait.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Traits;
+
+use App\Models\LogUserActivity;
+
+trait LogUserActivityTrait
+{
+    public function saveLogActivity($peopleId, $device, $request = null)
+    {
+        if ($request != null) {
+            $request = str_replace('\n', '', json_encode($request->input('query')));
+            $request = str_replace('{', '/', $request);
+            $request = str_replace('}', '', $request);
+            $request = explode('(', $request);
+            $request = preg_replace("/[[:blank:]]+/"," ",$request[0]);
+        }
+
+        $log            = new LogUserActivity();
+        $log->people_id = $peopleId;
+        $log->device    = $device;
+        $log->query     = $request;
+        $log->save();
+    }
+}
