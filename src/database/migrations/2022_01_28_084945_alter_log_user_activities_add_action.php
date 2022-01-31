8052d1e72b1282bdc48d5f93cad4aef49e91237e
Indra Prasetya <indraprasetya154@gmail.com>
2022-01-31 09:11:36 +0700

    Adjust table log and modify handle log activity

---
+++ src/database/migrations/2022_01_28_084945_alter_log_user_activities_add_action.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterLogUserActivitiesAddAction extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('log_user_activities', function (Blueprint $table) {
+            $table->string('query', 10)->change();
+            $table->string('action', 255)->after('query');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('log_user_activities', function (Blueprint $table) {
+            $table->dropColumn('action');
+            $table->text('query')->change();
+        });
+    }
+}
