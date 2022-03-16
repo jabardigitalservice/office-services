eec3beb88f38e7554667ce4885be74d1fd66da16
samudra-ajri <54126488+samudra-ajri@users.noreply.github.com>
2022-03-16 11:49:41 +0700

    Add action label: CORRECTION

---
+++ src/app/Enums/ActionLabelTypeEnum.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Enums;
+
+use Spatie\Enum\Enum;
+
+/**
+ * @method static self REVIEWED()
+ * @method static self CORRECTION()
+ */
+
+final class ActionLabelTypeEnum extends Enum
+{
+}
