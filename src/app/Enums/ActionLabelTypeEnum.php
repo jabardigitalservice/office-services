cb047114be5c630b0c90ab934a6fccea8f372d48
samudra-ajri <54126488+samudra-ajri@users.noreply.github.com>
2022-03-16 12:59:29 +0700

    Add action label: FINISHED

---
+++ src/app/Enums/ActionLabelTypeEnum.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Enums;
+
+use Spatie\Enum\Enum;
+
+/**
+ * @method static self REVIEWED()
+ * @method static self CORRECTION()
+ * @method static self FINISHED()
+ */
+
+final class ActionLabelTypeEnum extends Enum
+{
+}
