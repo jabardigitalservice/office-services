52e9ee662edafa82475e48d58fef5a53e6322b7a
samudra-ajri <54126488+samudra-ajri@users.noreply.github.com>
2022-04-07 14:47:09 +0700

    Add eksternal drafts filters and search Add filtering by its read and distributed status Add searching the list by its file name

--- src/app/Models/DocumentSignatureForward.php
+++ src/app/Models/DocumentSignatureForward.php
@@ -48,4 +48,73 @@ class DocumentSignatureForward extends Model
     {
         return $this->belongsTo(DocumentSignature::class, 'ttd_id', 'id');
     }
+
+    /**
+     * Search the list by its file name
+     *
+     * @param Object $query
+     * @param Array $search
+     *
+     * @return Object
+     */
+    public function search($query, $search)
+    {
+        $query->whereIn(
+            'ttd_id',
+            fn($query) => $query->select('id')
+                ->from('m_ttd')
+                ->where('nama_file', 'LIKE', '%' . $search . '%')
+        );
+
+        return $query;
+    }
+
+    /**
+     * Filtering the list
+     *
+     * @param Object $query
+     * @param Array $filter
+     *
+     * @return Object
+     */
+    public function filter($query, $filter)
+    {
+        $this->filterByReadStatus($query, $filter);
+        $this->filterByDistributionStatus($query, $filter);
+        return $query;
+    }
+
+     /**
+     * Filtering list by read status
+     *
+     * @param Object $query
+     * @param Array $filter
+     *
+     * @return Void
+     */
+    protected function filterByReadStatus($query, $filter)
+    {
+        $isRead = $filter['isRead'] ?? null;
+        if ($isRead || $isRead == '0') {
+            $arrayIsRead = explode(', ', $isRead);
+            $query->whereIn('is_read', $arrayIsRead);
+        }
+    }
+
+    /**
+     * Filtering list by distribution status
+     *
+     * @param Object $query
+     * @param Array $filter
+     *
+     * @return Void
+     */
+    protected function filterByDistributionStatus($query, $filter)
+    {
+        $isDistributed = $filter['isDistributed'] ?? null;
+        if ($isDistributed || $isDistributed == '0') {
+            $arrayIsDistributed = explode(', ', $isDistributed);
+            $query->whereIn('status', $arrayIsDistributed);
+        }
+    }
 }
